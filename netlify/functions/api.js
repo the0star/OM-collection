daa1033be0eef038203f1f2f1c364f5e5da89bdd
the0star <67977772+the0star@users.noreply.github.com>
2025-01-06 22:26:24 -0500

    Update for netlify deployment

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,11 +1,6 @@
-import express, { Router } from "express";
 import serverless from "serverless-http";
+import app from "../../app";
 
-const api = express();
+console.log("Hello from api.js");
 
-const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
-
-api.use("/api/", router);
-
-export const handler = serverless(api);
+export const handler = serverless(app);
