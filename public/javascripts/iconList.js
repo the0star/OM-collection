19cc27ae8c2486601ca548a41a97622122dad0f2
the0star <67977772+the0star@users.noreply.github.com>
2025-08-22 23:20:48 -0400

    Fix card link

--- public/javascripts/iconList.js
+++ public/javascripts/iconList.js
@@ -3,7 +3,7 @@ $(function () {
         .filter((x) => x.type === "Demon")
         .forEach((card, i) => {
             let cardName = encodeURIComponent(
-                card.name.replace(/_/g, "_").replace(/:/g, ",")
+                card.name.replace(/ /g, "_").replace(/:/g, ",")
             );
             let text = `<div class="col-6 col-md-4 col-lg-3 py-2 px-3 text-center"><a href="/card/${cardName}">
     <img class="mr-2" src="https://obey-me.fandom.com/wiki/Special:Redirect/file/${cardName}_icon.png?width=128" onerror="this.src='/images/reward_placeholder.png'" style="width:46%;height:auto;">
@@ -16,7 +16,7 @@ $(function () {
         .forEach((card, i) => {
             if (card.rarity !== "SR") {
                 let cardName = encodeURIComponent(
-                    card.name.replace(/_/g, "_").replace(/:/g, ",")
+                    card.name.replace(/ /g, "_").replace(/:/g, ",")
                 );
                 let characterNum = card.characters.indexOf(character) + 1;
                 let text = `<div class="col-3 col-md-2 p-2 text-center"><a href="/card/${cardName}">
